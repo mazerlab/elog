65636df02a29dea04c76897cce47dba29c64d711
James Mazer <james.mazer@yale.edu>
2014-12-15 16:18:07 -0500

    - removed svn refs from Makefile - centralized database settings into one file: dbsettings.py

---
+++ dbsettings.py
@@ -0,0 +1,9 @@
+
+# global parameters for talking to the sql server -- customize here
+# NOTE: these can also be overridden using environment vars by user
+
+HOST   = 'sql.mlab.yale.edu'
+PORT   = '3306'
+DB     = 'mlabdata'
+USER   = 'mlab'
+PASSWD = 'mlab'
