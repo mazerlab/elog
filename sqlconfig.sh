a075a153a035fd8178f19b856f219ad2f76e57c9
Jamie Mazer <mazerj2006@gmail.com>
2017-09-08 11:59:10 -0600

    always make for sql.mlab db host

--- sqlconfig.sh
+++ sqlconfig.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+cat <<EOF
+HOST='sql';
+USER='mlab';
+PASS='mlab';
+DB='mlabdata';
+PORT='3306';
+DIR='$1';
+EOF
+
+
+exit 0
+
+####################################################
+
+
 if [ "$(domainname)" = "mlab" ]; then
     cat <<EOF
 HOST='sql';
